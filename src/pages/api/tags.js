fdf5d61adc3b0721f3750e74f0925e18a91236a1
iamjaseer <42886352+iamjaseer@users.noreply.github.com>
2024-12-12 04:42:47 -0500

    Update tags.js

--- src/pages/api/tags.js
+++ src/pages/api/tags.js
@@ -1,17 +1,19 @@
 
 import axios from 'axios';
-import { woocommerceConsumerKey, woocommerceConsumerSecret, wordpressRestApiUrlWoocommerce } from "@/utils/variables";
+import { woocommerceConsumerKey, woocommerceConsumerSecret, wordpressRestApiUrlWoocommerceCustom } from "@/utils/variables";
 
 const Wordpress = axios.create({
-  baseURL: wordpressRestApiUrlWoocommerce,
+  baseURL: wordpressRestApiUrlWoocommerceCustom,
 });
 
 // Handler function
 export default async function handler(req, res) {
  
   try {
+
+   //  const response = await Wordpress.get(`products/tags?consumer_key=${woocommerceConsumerKey}&consumer_secret=${woocommerceConsumerSecret}&per_page=100`
     // Fetch products filtered by slug
-    const response = await Wordpress.get(`products/tags?consumer_key=${woocommerceConsumerKey}&consumer_secret=${woocommerceConsumerSecret}&per_page=100`
+    const response = await Wordpress.get(`product-tags`
     );
 
     // Return the filtered products
